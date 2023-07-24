4a24a77136b30731d97fe0f1f4aaebbb9dd2bf4a
walidramadan12 <107707564+walidramadan12@users.noreply.github.com>
2023-07-25 00:08:01 +0300

    update js
    brain lagged :)

--- login page/asstes/JS/script.js
+++ login page/asstes/JS/script.js
@@ -8,23 +8,26 @@ const passwords = [
     234,
     345,
 ]
-let username01 = admin01
+let username01 = "admin01"
 let password01 = 123
-let username02 = admin02
+let username02 = "admin02"
 let password02 = 234
-let username03 = admin03
+let username03 = "admin03"
 let password03 = 345
 let usernameEl = document.getElementById("Username")
 let passwordEl = document.getElementById("Password")
 let loginBtn = document.getElementById("Login-btn")
 
 loginBtn.addEventListener("click",function login(){
-    if (username01==password01 || username02==password02 || username03==password03 ) {
-        this.innerHTML
+    if (username01=="admin01" && password01=="123" || username02=="admin02" && password02=="234" || username03=="admin03" && password03=="345" ) {
+        alert("you have successfully loged in")
+
+        function newSecondsPage(){
+            window.location.href="welcome.html,3000"
+            window.location.href="list.html"
+        }
         
     }
-    else{
-        prompt("you still have", counter,"attempts")
-    }
+    
 })
 
